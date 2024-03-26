0de71966513dd97995e54cd9a853111dad4579de
saahiil-lim <saahiil@limosys.com>
2024-03-26 15:41:16 -0400

    testing jsdoc

--- lib/components/Buttons/Button.tsx
+++ lib/components/Buttons/Button.tsx
@@ -1,32 +1,28 @@
 import  { ButtonHTMLAttributes, ReactNode } from 'react';
 import PropTypes from 'prop-types';
-
 /**
  * Button component
  * @param {object} props - Component props
  * @param {ReactNode} props.leftDecor - Left decorator for the button
  * @param {ReactNode} props.rightDecor - Right decorator for the button
+ * @param {string} props.text - Text content for the button (required)
  * @returns {JSX.Element} Button component
  */
 interface ButtonBaseProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   leftDecor?: ReactNode;
   rightDecor?: ReactNode;
+  text: string;
 }
 
-const Button = ({leftDecor, rightDecor ,...props}: ButtonBaseProps) => {
+const Button = ({ leftDecor, rightDecor, text, ...props }: ButtonBaseProps) => {
 
   return (
     <div>
       {rightDecor && rightDecor}
-      <button {...props}>ButtonText</button>
+      <button {...props}>{text}</button>
       {leftDecor && leftDecor}
     </div>
   );
-};
-
-Button.propTypes = {
-  leftDecor: PropTypes.node,
-  rightDecor: PropTypes.node,
 };
 
 export { Button };
--- lib/components/Input/Input.jsx
+++ lib/components/Input/Input.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
 /**
  * Input component
+ *  * @component
+ * @example
+ * // Example usage:
+ * // Renders a basic input field with a placeholder and custom class name.
+ * <Input
+ *   className="custom-input"
+ *   placeholder="Enter your name"
+ * />
+ * 
  * @param {object} props - Component props
  * @param {string} [props.className] - Class name for the input element
  * @returns {JSX.Element} Input component
